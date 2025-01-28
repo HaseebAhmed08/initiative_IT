1217462ddd3c8dd1f574e36acd0c981fa3176ebb
HaseebAhmed08 <ha9719121@gmail.com>
2025-01-28 21:55:47 +0500

    python work updated

---
+++ tasks_gov_it/data_types.py
@@ -0,0 +1,27 @@
+# this is string
+x = "Hello World"		
+
+# this is intiger
+x = 20		
+
+# this is  float
+x = 20.5		
+
+# this is  complex type
+x = 1j		
+
+# this is list type
+x = ["apple", "banana", "cherry"]		
+#  this is tuple type
+x = ("apple", "banana", "cherry")		
+
+# this is range 
+x = range(6)
+
+# this is dictionary type
+x = {"name" : "John", "age" : 36}		
+
+x = {"apple", "banana", "cherry"}		
+
+# this is bool type
+x = True	
