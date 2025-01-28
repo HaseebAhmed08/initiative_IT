f777c4ca32de9518459680d9825e7a9b68efb2cf
HaseebAhmed08 <ha9719121@gmail.com>
2025-01-28 09:34:35 +0500

    i uploded file structure of variables in python

---
+++ variables_in_python/structure_of_variables.py
@@ -0,0 +1,46 @@
+
+
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+# if you want to write name in variable  then you will write any name 
+# in "  " these inverted comas  that will called string variable 
+
+myname :str = 'haseeb ahmed '
+
+Myname :str = 'haseeb ahmed'
+
+MY_Father_Name : str = 'naeem ahmed'
+
+grade : str = 'A'
+
+city_name : str = 'Karachi'
+
+
+# if you want to write number in  variable  then you will write any numeric 
+#   that will called intiger  variable 
+# like this 
+
+
+Age : int = 22
+
+price_of_car : int = 1222222
+
+
+
+
+# variable  in true false
+
+is_student : bool = True
+
+is_open : bool = False
+
+is_teacher : bool = True
+
+
+
+
